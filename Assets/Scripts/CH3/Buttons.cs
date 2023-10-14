886f9c7aae58e38f2cfa19b7c34673202787bf37
KeremTancan <tancankerem149@gmail.com>
2023-10-15 01:07:20 +0300

    Finished: CH3

---
+++ Assets/Scripts/CH3/Buttons.cs
@@ -0,0 +1,91 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buttons : MonoBehaviour
+{
+    public GameObject EvetSprite;
+    public GameObject HayýrSprite;
+
+    private int sayý =  0;
+
+    void Start()
+    {
+        HayýrSprite.SetActive(true);
+        EvetSprite.SetActive(false);
+    }
+
+    
+
+    public void EvetTik()
+    {
+        GameObject gameObjectToHide = GameObject.Find("");
+        HayýrSprite.SetActive(false);
+        EvetSprite.SetActive(true);
+        
+
+        switch (sayý)
+        {
+            case 0: gameObjectToHide = GameObject.Find("Hilafet");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+            case 1:
+                gameObjectToHide = GameObject.Find("Soyadý");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+            case 2:
+                gameObjectToHide = GameObject.Find("Seçilme");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+            case 3:
+                gameObjectToHide = GameObject.Find("Yunanistan");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+
+        }
+
+
+    }
+
+    public void HayirTik()
+    {
+        GameObject gameObjectToHide = GameObject.Find("");
+        HayýrSprite.SetActive(true);
+        EvetSprite.SetActive(false);
+        
+
+        switch (sayý)
+        {
+            case 0:
+                gameObjectToHide = GameObject.Find("Hilafet");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+            case 1:
+                gameObjectToHide = GameObject.Find("Soyadý");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+            case 2:
+                gameObjectToHide = GameObject.Find("Seçilme");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+            case 3:
+                gameObjectToHide = GameObject.Find("Yunanistan");
+                Destroy(gameObjectToHide, 1);
+                sayý++;
+                break;
+
+        }
+
+        if (gameObjectToHide == null)
+        {
+            
+        }
+    }
+}
